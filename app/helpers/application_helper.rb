d095314f5b3ecf7425fd9314f554d117369c2d0c
Saulo Oliveira <me@saulo.pro>
2024-02-07 13:51:13 +0000

    Add Google Analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def ga_measurement_id
+    ENV[:GA_MEASUREMENT_ID] if ENV[:GA_MEASUREMENT_ID].present?
+
+    raise "Missing GA_MEASUREMENT_ID environment variable"
+  end
 end
